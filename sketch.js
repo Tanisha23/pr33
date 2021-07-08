9ad884781ca69f02485abc8885045e5167ca11d8
Tanisha23 <77339985+Tanisha23@users.noreply.github.com>
2021-07-08 12:45:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg,music;
+var girl;
+var snow,olaf;
+var snowball1
+function preload(){
+  backgroundImg = loadImage("snow2.jpg");
+  music=loadSound("music.mp3")
+}
+
+function setup(){
+    var canvas = createCanvas(2000,700);
+    engine = Engine.create();
+    world = engine.world;
+
+    snowball=new Snowball(random(50, 350),40, 10, 10)
+    snowball1=new Snowball(random(0, 1000),random(50, 500), 30, 30)
+    snowball2=new Snowball(random(0, 1000),random(50, 500), 30, 30)
+    snowball3=new Snowball(random(0, 1000),random(50, 500), 30, 30)
+    snowball5=new Snowball(random(0, 1000),random(50, 500), 30, 30)
+
+    ground = new Ground(600,height,1200,20);
+    girl = new Girl(900,100,70,70);
+    olaf = new Olaf(200,200,80,80);
+    snow = new Snow(200,200,80,80);
+    console.log(girl)
+
+}
+
+function draw(){
+    background(backgroundImg)
+    Engine.update(engine);
+ 
+    if (frameCount % 100 == 0) {
+      snowball1=new Snowball(random(0, 1000),random(50, 500), 30, 30)
+
+    }
+    if (frameCount % 200 == 0) {
+      snowball2=new Snowball(random(70, 1500),random(50, 1000), 30, 30)
+
+    }
+    ground.display();
+      girl.display();
+    
+     olaf.display();
+     snow.display();
+     snowball.display();
+     snowball1.display();
+
+
+   //  music.play() ;   
+}
